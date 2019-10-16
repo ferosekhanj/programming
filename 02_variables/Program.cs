19818c80ed45a6e280fe7a133d33be0678ba8019
Ferose khan, Jeberla <j.ferosekhan@siemens-healthineers.com>
2019-10-16 08:27:20 +0530

    Added session on variables.

---
+++ 02_variables/Program.cs
@@ -0,0 +1,16 @@
+class Program
+{
+	static void Main()
+	{
+		var ageInYears = 10; 
+		var weightInKgs = 30; 
+		var nameOfChild = "Jane"; 
+		System.Console.WriteLine($"{ageInYears} {weightInKgs} {nameOfChild}");
+		
+		ageInYears = 12;
+		weightInKgs = 35;
+		nameOfChild = "Jane";
+		System.Console.WriteLine($"{nameOfChild} is of age {ageInYears} and of weight {weightInKgs} Kgs ");
+		
+	}
+}
